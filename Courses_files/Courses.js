784b9e7cb22523bf65ad2fcc54d3febb9dd5ffeb
Mark Broadhurst <marklee.broadhurst@gmail.com>
2017-01-20 02:21:45 +0000

    New FIC Newbury Website
    Created 19.01.17, Committed to Git Hub on 20.01.17

---
+++ Courses_files/Courses.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20170120
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Courses_files/notebook_ul.png'),IWCreateImage('Courses_files/notebook_top.png'),IWCreateImage('Courses_files/notebook_ur.png'),IWCreateImage('Courses_files/notebook_right.png'),IWCreateImage('Courses_files/notebook_lr.png'),IWCreateImage('Courses_files/notebook_bottom.png'),IWCreateImage('Courses_files/notebook_ll.png'),IWCreateImage('Courses_files/notebook_left.png')],null,0,1.000000,3.000000,2.000000,1.000000,3.000000,18.000000,16.000000,17.000000,19.000000,76.000000,123.000000,79.000000,122.000000,null,null,null,0.400000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Courses_files/CoursesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
