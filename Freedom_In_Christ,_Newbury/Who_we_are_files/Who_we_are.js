2d0cd00cf129d03a4a669b7dd68dc81f8102b49c
Mark Broadhurst <marklee.broadhurst@gmail.com>
2017-01-21 15:03:48 +0000

    Updates
    New logo added
    Web links added to graphics
    Bridge Church detail added

--- Freedom_In_Christ,_Newbury/Who_we_are_files/Who_we_are.js
+++ Freedom_In_Christ,_Newbury/Who_we_are_files/Who_we_are.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20170120
+// Created by iWeb 3.0.4 local-build-20170121
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Who_we_are_files/notebook_ul.png'),IWCreateImage('Who_we_are_files/notebook_top.png'),IWCreateImage('Who_we_are_files/notebook_ur.png'),IWCreateImage('Who_we_are_files/notebook_right.png'),IWCreateImage('Who_we_are_files/notebook_lr.png'),IWCreateImage('Who_we_are_files/notebook_bottom.png'),IWCreateImage('Who_we_are_files/notebook_ll.png'),IWCreateImage('Who_we_are_files/notebook_left.png')],null,0,1.000000,3.000000,2.000000,1.000000,3.000000,18.000000,16.000000,17.000000,19.000000,76.000000,123.000000,79.000000,122.000000,null,null,null,0.400000)});registry.applyEffects();}
