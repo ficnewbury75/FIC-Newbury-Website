c5ce7dde10ff6096d52e6a8a01be229aaafcc70a
Mark Broadhurst <marklee.broadhurst@gmail.com>
2017-03-08 17:03:45 +0000

    Website deleted
    Redirection page left to main UK office. 08.03.17

--- Freedom_In_Christ,_Newbury/WEBSITE_CLOSED_files/WEBSITE_CLOSED.js
+++ Freedom_In_Christ,_Newbury/WEBSITE_CLOSED_files/WEBSITE_CLOSED.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20170121
+// Created by iWeb 3.0.4 local-build-20170308
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
+{loadMozillaCSS('WEBSITE_CLOSED_files/WEBSITE_CLOSEDMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
